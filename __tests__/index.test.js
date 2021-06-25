46d46a30d17988014a2cf541edb1f22659804902
Polina Zhukovskaya <mayabessonova_16_94@mail.ru>
2021-06-25 12:55:47 +0300

    step4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,20 @@
+/* eslint-disable no-underscore-dangle */
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
+// import { dirname } from 'path';
+import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
- const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-
-
 test('genDiff JSON json', () => {
-    const firstFilePath = getFixturePath('file1.json');
-    const secondFilePath = getFixturePath('file2.json');
-    const thirdFilePath = getFixturePath('after.json');
-    const thirdFile = readFile(thirdFilePath);
-    expect(genDiff(firstFilePath, secondFilePath)).toEqual(thirdFile);
-}); 
+  const firstFilePath = getFixturePath('file1.json');
+  const secondFilePath = getFixturePath('file2.json');
+  // const thirdFilePath = getFixturePath('after.json');
+  const thirdFile = readFile('after.json');
+  expect(genDiff(firstFilePath, secondFilePath)).toEqual(JSON.parse(thirdFile));
+});
